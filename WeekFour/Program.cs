75016ee5cde482659bab7c1acdeaa65d409f1820
Brandon Gunthner <typhus733@gmail.com>
2021-10-06 20:48:16 -0500

    Initial Commit

---
+++ WeekFour/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WeekFour
+{
+    class Program
+    {
+
+        static int GetMax(int nOne, int nTwo)
+        {
+            int max = 0;
+            if (nOne > nTwo)
+            {
+                max = nOne;
+            }
+
+            else
+            {
+                 max = nTwo;
+            }
+
+            return max;
+        }
+
+        static void NumberTwo()
+        {
+            Console.WriteLine("Enter the first number");
+            int intOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int intTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third number");
+            int intThree = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMax(GetMax(intOne, intTwo), intThree));
+        
+        }
+
+
+        static void Main(string[] args)
+        {
+            NumberTwo();
+        }
+    }
+}
